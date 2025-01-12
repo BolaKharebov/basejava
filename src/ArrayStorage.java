c57e8500b92e897ca1744f9ef4d3ca6e2458d6cf
Kharebov Bola <kharebovbola@gmail.com>
2025-01-13 02:34:10 +0300

    Implemented all the methods

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -3,28 +3,51 @@
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
+    int index = 0;
 
     void clear() {
+        while (index != 0) {
+            storage[index] = null;
+            index--;
+        }
     }
 
     void save(Resume r) {
+        storage[index] = r;
+        index++;
     }
 
     Resume get(String uuid) {
-        return null;
+        Resume resume = new Resume();
+        resume.uuid = uuid;
+        return resume;
     }
 
     void delete(String uuid) {
+        for (int i = 0; i < index; i++) {
+            if (storage[i].toString().equals(uuid)) {
+                storage[i] = null;
+                index--;
+                if (i != index) {
+                    storage[i] = storage[size()];
+                    storage[size()] = null;
+                }
+            }
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        Resume[] array = new Resume[index];
+        for (int i = 0; i < index; i++) {
+            array[i] = storage[i];
+        }
+        return array;
     }
 
     int size() {
-        return 0;
+        return index;
     }
 }
